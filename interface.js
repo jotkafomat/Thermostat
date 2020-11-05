4c70ef9e66281b1cca74c8d6a203baaedd1dcb01
jotkafomat <k-j@xl.wp.pl>
2020-11-05 20:44:41 +0000

    add acion to the buttons

--- interface.js
+++ interface.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
 var thermostat = new Thermostat();
-$('#temperature').text(thermostat.temperature);
-})
+updateTemperature();
+
+$('#up').click(function () {
+  thermostat.up();
+  updateTemperature();
+});
+
+$('#down').click(function () {
+  thermostat.down();
+  updateTemperature();
+});
+
+$('#reset').click(function () {
+  thermostat.resetTemperature();
+  updateTemperature();
+});
+
+$('#pwsON').click(function () {
+  thermostat.switchPowerSavingModeOn();
+  $('#power-saving-status').text('on');
+  updateTemperature();
+});
+
+$('#pwsOff').click(function () {
+  thermostat.switchPowerSavingModeOff();
+  $('#power-saving-status').text('off');
+  updateTemperature();
+});
+
+  function updateTemperature(){
+    $('#temperature').text(thermostat.temperature);
+  };
+});
+
+
+// $('element').on('event', function() {
+//
+// })
